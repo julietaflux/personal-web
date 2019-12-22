1e05ee95a8f9debed0a92fe667c50b0d966c6be6
julietaflux <julietaflux@gmail.com>
2019-12-22 19:59:37 -0300

    AEGH

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+    <v-content>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="8" md="4">
+            <HelloWorld />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+
+  components: {
+    HelloWorld
+  },
+
+  data: () => ({
+    //
+  })
+};
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container fluid>
+    <p class="font-weight-light display-1 text-center">Hi there</p>
+    <p class="font-weight-light text-center">This is Jules Flux from Galaxy 934-F AKA Pandororbit.</p>
+  </v-container>
+</template>
+
+
+<script>
+export default {
+  name: "HelloWorld",
+
+  data: () => ({
+    Links: [
+      {
+        text: "linked-in",
+        href: "https://www.linkedin.com/in/julietaflux/"
+      },
+      {
+        text: "github",
+        href: "https://github.com/julietaflux"
+      }
+    ]
+  })
+};
+</script>
