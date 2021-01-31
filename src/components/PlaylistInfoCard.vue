9040165ed1e5e48c7686549a93554e3c582bbfef
julietaflux <julietaflux@gmail.com>
2021-01-31 18:27:50 -0300

    JF more stuff

--- src/components/PlaylistInfoCard.vue
+++ src/components/PlaylistInfoCard.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="body-2 text-justify ">
     <p class="font-semibold text-2xl select-none">{{ title }}</p>
+    <ul class="justify-center">
+      <li class="my-3">
+        <span class="flex text-sm font-bold">
+          {{ artists }}
+        </span>
+      </li>
+    </ul>
     <p class="mt-2 mb-4 text-sm md:text-lg select-none">{{ description }}</p>
     <ul class="justify-center">
       <li class="mb-3">
@@ -21,12 +28,7 @@
           Album art
         </span>
       </li>
-      <li class="mb-3">
-        <span class="flex text-sm font-bold cursor-pointer">
-          <IconMusic />
-          {{ artists }}
-        </span>
-      </li>
+
       <li>
         <a :href="publicUrl" target="_blank" rel="noopener noreferrer">
           <span class="flex text-sm font-bold cursor-pointer">
@@ -38,7 +40,7 @@
     </ul>
     <div class="my-5 md:lg-0">
       <template v-for="tag in tags" :key="tag">
-        <span class="underline mx-1 capitalize">#{{ tag }}</span>
+        <span class="underline mx-1">#{{ tag }}</span>
       </template>
     </div>
   </div>
@@ -47,11 +49,10 @@
 <script>
 import IconPlay from "../components/icons/play";
 import IconBolt from "../components/icons/bolt";
-import IconMusic from "../components/icons/music";
 import IconSpotify from "../components/icons/spotify";
 export default {
   name: "playlistinfocard",
-  components: { IconPlay, IconBolt, IconMusic, IconSpotify },
+  components: { IconPlay, IconBolt, IconSpotify },
   props: ["title", "description", "tags", "publicUrl", "artists"],
 };
 </script>
